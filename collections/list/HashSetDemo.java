520d78260254f5cb69d8fe711831afd95908fd41
ShraddhaS10 <shraddhasingh4964@gmail.com>
2024-06-26 11:16:45 +0530

    Update HashSetDemo.java

--- collections/list/HashSetDemo.java
+++ collections/list/HashSetDemo.java
@@ -20,4 +20,50 @@ public class HashSetDemo {
 		intSet.forEach(System.out::println);
 	}
 }
- 
+
+
+
+//new prg
+package com.gv.collections.list;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.gv.collections.entities.Employee;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+//		Set<Integer> intSet = new HashSet<>();
+//		intSet.add(4);
+//		intSet.add(2);
+//		intSet.add(3);
+//		intSet.add(2);
+////		intSet.add(4);
+//		intSet.add(null);
+//		intSet.add(1);
+//		
+//	//	System.out.println(intSet.size());
+//		
+//		intSet.forEach(System.out::println);
+		
+		Set<Employee> employeeSet = new HashSet<>();
+		
+		employeeSet.add(new Employee(1,"NImisha"));
+		employeeSet.add(new Employee(1,"NimishA"));
+		employeeSet.add(new Employee(1,"NImisha"));
+		employeeSet.add(new Employee(1,"Nimisha"));
+		employeeSet.add(new Employee(1,"NiMisha"));
+		employeeSet.add(new Employee(1,"NimiSha"));
+		
+		
+		System.out.println();
+		
+		System.out.println(new Employee(1,"Nimisha").hashCode());
+		System.out.println(new Employee(1,"Nimisha").hashCode());
+		System.out.println(new Employee(1,"Nimisha").hashCode());
+		System.out.println(employeeSet);
+		
+		employeeSet.forEach(System.out::println);
+	}
+}
